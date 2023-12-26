7c65567e6d1f27c34bca5f5871855a8dc3340c15
Jooyeong(extrayugen) <supershicker@gmail.com>
2023-12-26 13:08:17 +0900

    Initial Commit

---
+++ Movie-Tickets-Booking-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-Tickets-Booking-1
+//
+//  Created by t2023-m0031 on 12/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
